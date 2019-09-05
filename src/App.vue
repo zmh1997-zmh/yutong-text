9ca186d0c3e767a2b2b9e6b1795553904cf7c0e3
zmh1997-zmh <zmh13874268303@163.com>
2019-09-05 09:28:00 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="app">
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Orgechart></Orgechart>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+import Orgechart from './components/Orgechart'
+export default {
+  name: 'app',
+  components: {
+    HelloWorld,
+    Orgechart
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
